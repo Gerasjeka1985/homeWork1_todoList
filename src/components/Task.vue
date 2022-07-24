fc78d296b68f5b1e2b644f8009bca8881c8d3c4b
GerasJeka <gekgrom@mail.ru>
2022-07-24 22:37:19 +0300

    added 24.07.22

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -18,10 +18,14 @@ const check = computed({
 </script>
 
 <template>
-  <div class="task-item">
+  <div v-if="!modelValue" class="task-item">
     <slot></slot>
     <input type="checkbox" v-model="check" />
-    <div v-if="modelValue">Задача завершена</div>
+  </div>
+  <div v-else class="task-done">
+    <slot></slot>
+    <input type="checkbox" v-model="check" />
+    <div>Задача завершена</div>
   </div>
 </template>
 
@@ -35,5 +39,17 @@ const check = computed({
   color: rgb(9, 9, 105);
   border: 1px solid rgb(9, 9, 105);
   border-radius: 5px;
+}
+
+.task-done {
+  margin-top: 10px;
+  margin-left: 20px;
+  width: 40%;
+  height: 40px;
+  background: none;
+  color: rgb(56, 5, 12);
+  border: 1px solid rgb(9, 9, 105);
+  border-radius: 5px;
+  text-decoration: line-through;
 }
 </style>
